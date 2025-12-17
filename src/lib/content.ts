6f8c94310995f88856f38aa8978d9af4578c0881
Minseo Lee <itoupluk427@gmail.com>
2025-12-17 18:45:02 +0900

    Update content.ts

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -15,6 +15,7 @@ export const blogPostMetadataSchema = v.object({
   slug: v.pipe(v.string(), v.trim()),
   title: v.pipe(v.string(), v.trim()),
   description: v.optional(v.pipe(v.string(), v.trim())),
+  source: v.optional(v.pipe(v.string(), v.url(), v.trim())),
   pubDate: v.pipe(v.date()),
   lastMod: v.optional(v.pipe(v.date())),
   tags: v.optional(
