82ceeff036937ed45c372f2221a1b1e7b3121529
Minseo Lee <itoupluk427@gmail.com>
2025-12-18 06:33:29 +0900

    Update +page.server.ts

--- src/routes/[category]/[slug]/+page.server.ts
+++ src/routes/[category]/[slug]/+page.server.ts
@@ -82,7 +82,7 @@ function figure() {
             type: 'element',
             tagName: 'figcaption',
             properties: {},
-            children: [{type: 'text', value: node.children[0].value}],
+            children: [{type: 'text', value: (node.children[0] as any).value}],
           },
         ]
       }
