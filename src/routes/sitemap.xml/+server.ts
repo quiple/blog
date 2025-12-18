e9d2f18d94f79712aa68bc049149ba49a473f47c
Minseo Lee <itoupluk427@gmail.com>
2025-12-19 03:35:41 +0900

    Update +server.ts

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,7 +2,7 @@ export async function GET() {
   return new Response(
     `
       <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       </urlset>
     `.trim(),
     {
