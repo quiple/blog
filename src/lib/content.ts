f6a2cab7d4a84d4ab15963c27521158196e0ab5b
Minseo Lee <itoupluk427@gmail.com>
2025-12-19 10:44:32 +0900

    Update content.ts

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -19,7 +19,7 @@ export const blogPostMetadataSchema = v.object({
   image: v.optional(v.pipe(v.string(), v.trim())),
   media: v.optional(v.pipe(v.string(), v.trim())),
   pubDate: v.pipe(v.string(), v.isoDateTime(), v.trim()),
-  transDate: v.optional(v.pipe(v.string(), v.isoDateTime(), v.trim())),
+  transDate: v.pipe(v.string(), v.isoDateTime(), v.trim()),
   tags: v.optional(
     v.pipe(
       v.string(),
@@ -63,7 +63,7 @@ export function getBlogPostsMetadata() {
 
       return getMetadataFromMatter(category, slug, data)
     })
-    .sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf())
+    .sort((a, b) => Date.parse(`${b.transDate.valueOf()}+09:00`) - Date.parse(`${a.transDate.valueOf()}+09:00`))
 
   return posts
 }
