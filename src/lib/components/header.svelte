17268a40ea6353f29197b7a713729a87cb2f09ed
Minseo Lee <itoupluk427@gmail.com>
2025-12-25 07:13:31 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -7,7 +7,7 @@
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index'
   import {Input} from '$lib/components/ui/input/index'
   import {isHero} from '$lib/stores/header'
-  import {toggleMode} from 'mode-watcher'
+  import {mode, toggleMode} from 'mode-watcher'
 
   let query = $state('')
   let inputElement = $state<HTMLInputElement | null>(null)
@@ -47,29 +47,24 @@
     </div>
 
     <div class="flex gap-2">
-      <!-- <DropdownMenu.Root>
+      <DropdownMenu.Root>
         <DropdownMenu.Trigger>
           {#snippet child({props})}
-            <Button {...props} size="icon" variant="secondary" aria-label="메뉴">
+            <Button {...props} size="icon" variant="ghost" aria-label="메뉴">
               <Menu />
             </Button>
           {/snippet}
         </DropdownMenu.Trigger>
         <DropdownMenu.Content align="end">
-          <DropdownMenu.Item onclick={() => goto('/login')}><LogIn /> 로그인</DropdownMenu.Item>
-          <DropdownMenu.Item onclick={() => goto('/user/quiple')}><User /> 프로필</DropdownMenu.Item>
-          <DropdownMenu.Item onclick={() => goto('/settings')}><Settings /> 설정</DropdownMenu.Item>
+          <DropdownMenu.Item onclick={toggleMode}>
+            {#if mode.current === 'dark'}
+              <Sun /> 라이트 테마
+            {:else}
+              <Moon /> 다크 테마
+            {/if}
+          </DropdownMenu.Item>
         </DropdownMenu.Content>
-      </DropdownMenu.Root> -->
-      <!-- <div class="relative hidden md:block">
-        <Search class="absolute top-2.5 left-2.5 z-10 h-4 w-4" />
-        <Input name="search" type="text" class="pl-8" bind:value={query} bind:ref={inputElement} placeholder="검색" />
-      </div> -->
-      <Button onclick={toggleMode} variant="secondary" size="icon">
-        <Sun class="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-        <Moon class="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-        <span class="sr-only">테마 변경</span>
-      </Button>
+      </DropdownMenu.Root>
     </div>
   </section>
 </header>
@@ -79,14 +74,10 @@
 
   header
     @apply relative md:sticky top-0 py-4 sm:py-6
-    :global(Button), :global(input)
-      @apply text-foreground dark:text-secondary-foreground backdrop-blur-xs
-    :global(Button)
-      @apply bg-ring/12 dark:bg-ring/25 hover:bg-ring/9.5 dark:hover:bg-ring/21
-    :global(input)
-      @apply bg-transparent dark:bg-ring/12 border-foreground/13 dark:border-foreground/15 placeholder:text-foreground/63 dark:placeholder:text-foreground/56
     &.hero
-      @apply text-(--hero-foreground-color)
+      @apply text-(--hero-foreground)
+      :global(Button):hover
+        @apply text-(--hero-foreground) bg-(--hero-foreground)/10
     section
       @apply relative container-x !max-w-full px-4 sm:!px-6 flex justify-between items-start gap-8
 </style>
