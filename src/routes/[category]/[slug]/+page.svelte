c1eb452306316c76c6acab1929a91bb118f707d7
Minseo Lee <itoupluk427@gmail.com>
2025-12-28 09:42:26 +0900

    Update +page.svelte

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -18,12 +18,18 @@
 
 <svelte:head>
   <title>{data.title}</title>
-  <meta property="og:title" content={data.title} />
   <meta name="description" content={data.description} />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content={data.canonicalURL} />
+  <meta property="og:title" content={data.title} />
   <meta property="og:description" content={data.description} />
+  <meta property="article:published_time" content={`${data.origDate}+09:00`} />
+
   <link rel="canonical" href={data.canonicalURL} />
   {@html `<script type="application/ld+json">${jsonLd}</script>`}
   {#if data.image}
+    <meta property="og:image" content={data.image} />
     {@html `<style>:root { --hero-foreground: #${data.imageForeground.toString()}; }</style>`}
   {/if}
 </svelte:head>
