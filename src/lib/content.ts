df55fcc40bfc802623843bb729d8dc8d56f37b71
Minseo Lee <itoupluk427@gmail.com>
2025-12-28 09:49:04 +0900

    Update content.ts

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -16,10 +16,10 @@ export const blogPostMetadataSchema = v.object({
   title: v.pipe(v.string(), v.trim()),
   description: v.optional(v.pipe(v.string(), v.trim())),
   source: v.optional(v.pipe(v.string(), v.url(), v.trim())),
+  origAuthor: v.optional(v.pipe(v.string(), v.url(), v.trim())),
   image: v.optional(v.pipe(v.string(), v.trim())),
   imageForeground: v.optional(v.pipe(v.string(), v.trim())),
   media: v.optional(v.pipe(v.string(), v.trim())),
-  origAuthor: v.optional(v.pipe(v.string(), v.url(), v.trim())),
   origDate: v.pipe(v.string(), v.isoDateTime(), v.trim()),
   pubDate: v.pipe(v.string(), v.isoDateTime(), v.trim()),
   tags: v.optional(
