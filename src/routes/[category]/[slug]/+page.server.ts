f012382efbf4e304698d617e7d60ee9b2f40ea86
Lee Minseo <itoupluk427@gmail.com>
2025-12-29 16:46:13 +0900

    Update +page.server.ts

--- src/routes/[category]/[slug]/+page.server.ts
+++ src/routes/[category]/[slug]/+page.server.ts
@@ -72,7 +72,7 @@ function figure() {
   return (tree: Root) => {
     visit(tree, (node) => {
       if (node.type === 'containerDirective' || node.type === 'leafDirective') {
-        if (node.name !== 'figure') return
+        if (node.name !== 'figure' && node.name !== 'youtube' && node.name !== 'spotify') return
 
         const data = node.data || (node.data = {})
         const attributes = node.attributes || {}
@@ -81,6 +81,11 @@ function figure() {
         const className = attributes.class ?? ''
         const img = `<img class="${cn('not-prose', className)}" src="${src}">`
         const youtube = `<iframe class="${cn('max-w-full w-xl 2xl:w-2xl aspect-video', className)}" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        const spotify = `<iframe class="${cn('max-w-full w-xl 2xl:w-2xl', className)}" data-testid="embed-iframe" src="https://open.spotify.com/embed/playlist/${id}" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+
+        let content = img
+        if (node.name === 'youtube') content = youtube
+        if (node.name === 'spotify') content = spotify
 
         data.hName = 'figure'
         node.children =
@@ -88,13 +93,13 @@ function figure() {
             ? [
                 {
                   type: 'html',
-                  value: `<div class="self-center">${id ? youtube : img}</div>`,
+                  value: `<div class="self-center">${content}</div>`,
                 },
               ]
             : [
                 {
                   type: 'html',
-                  value: `<div class="self-center">${id ? youtube : img}</div><figcaption>`,
+                  value: `<div class="self-center">${content}</div><figcaption>`,
                 },
                 // @ts-ignore
                 ...node.children[0].children,
