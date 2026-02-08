4b4fe60db8693a8c96c03a9913f7395d7939f0ef
Minseo Lee <itoupluk427@gmail.com>
2026-02-08 16:33:49 +0900

    font-range

---
+++ subset.js
@@ -0,0 +1,20 @@
+import {fontRange} from 'font-range'
+
+const kr_path = 'node_modules/@ibm/plex-sans-kr/fonts/complete/woff2/hinted/IBMPlexSansKR-'
+const jp_path = 'node_modules/@ibm/plex-sans-jp/fonts/complete/woff2/hinted/IBMPlexSansJP-'
+
+fontRange(kr_path + 'ExtraLight', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'Light', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'Regular', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'Text', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'Medium', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'SemiBold', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+fontRange(kr_path + 'Bold', 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap', 'static/fonts')
+
+fontRange(jp_path + 'ExtraLight', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'Light', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'Regular', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'Text', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'Medium', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'SemiBold', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
+fontRange(jp_path + 'Bold', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', 'static/fonts')
