5bcb6ea6cefcdae924784e6dacc6c1b7c7f1880a
Minseo Lee <itoupluk427@gmail.com>
2026-02-25 14:12:55 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,15 +22,19 @@
   <h1 class="text-2xl leading-tight font-bold tracking-tight md:text-3xl lg:text-4xl mb-2 md:mb-4">전체 글</h1>
   <ul class="flex flex-col">
     {#each data.posts as post}
+      {@const displayDate =
+        'origDate' in post && post.origDate instanceof Date
+          ? post.origDate
+          : new Date(`${'origDate' in post && typeof post.origDate === 'string' ? post.origDate : post.pubDate}+09:00`)}
       <li>
         <a href={post.relativeURL} class="list-item">
           <div class="grow z-1">
             <strong class="line-clamp-1 mb-1" use:transition={`post-title-${post.slug}`}>{post.title}</strong>
             <p class="text-sm line-clamp-3 mb-1 text-justify">{post.description}</p>
             <small class="text-muted-foreground" use:transition={`post-metadata-${post.slug}`}>
-              {#if post.media}
+              {#if 'media' in post && post.media}
                 {post.media}&#8194;&bullet;&#8194;{/if}{new Intl.DateTimeFormat('ko-KR', {dateStyle: 'long'}).format(
-                typeof post.origDate === 'object' ? post.origDate : Date.parse(`${post.origDate}+09:00`),
+                displayDate,
               )}
             </small>
           </div>
