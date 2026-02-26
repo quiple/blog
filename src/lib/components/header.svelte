d60be50f00c965ac286ce5c66fd3f682befc02b0
Minseo Lee <itoupluk427@gmail.com>
2026-02-26 20:23:23 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -93,7 +93,10 @@
         @apply before:opacity-100
       .logo
         mask-image: if(
-          style(--hero-foreground: #fff): if(style(--outline-color): none;else: var(--svgGradeDown));
+          style(--hero-foreground: #fff): if(
+            style(--outline-color): none;
+            else: var(--svgGradeDown);
+          );
           else: none;
         ) !important
     section
