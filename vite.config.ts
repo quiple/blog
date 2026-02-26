3cd13774a73088daa9688c8eddd47c05ece9054c
Minseo Lee <itoupluk427@gmail.com>
2026-02-27 04:40:41 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
+import path from 'path'
 import {sveltekit} from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import {defineConfig} from 'vite'
 
-export default defineConfig({plugins: [tailwindcss(), sveltekit()], assetsInclude: ['**/*.md']})
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  assetsInclude: ['**/*.md'],
+  resolve: {
+    alias: [
+      {
+        find: /^remark-github-alerts$/,
+        replacement: path.resolve('node_modules/remark-github-alerts/src/index.ts'),
+      },
+    ],
+  },
+})
