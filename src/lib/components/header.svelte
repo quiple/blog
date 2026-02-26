4da9e03e8b93a57b9285dfd55188d9de5c155aad
Minseo Lee <itoupluk427@gmail.com>
2026-02-26 18:42:24 +0900

    svgGradeDown

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -2,8 +2,9 @@
   import {Menu, Moon, Search, Sun} from '@lucide/svelte'
   import {goto} from '$app/navigation'
   import {page} from '$app/state'
+  import svgGradeDown from '$lib/assets/logo-grade-down.svg'
+  import svgOutline from '$lib/assets/logo-outline.svg'
   import menu from '$lib/assets/menu.svg'
-  import qLogo from '$lib/assets/q-logo.svg'
   import Q from '$lib/components/q.svelte'
   import {Button} from '$lib/components/ui/button/index'
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index'
@@ -46,7 +47,7 @@
 <header class={headerClassName} use:transition={'header'}>
   <section>
     <div class="flex gap-2">
-      <a href="/" class="logo" style={`--svg: url("${qLogo}")`}>
+      <a href="/" class="logo" style={`--svgOutline: url("${svgOutline}"); --svgGradeDown: url("${svgGradeDown}")`}>
         <Q class="w-9" />
       </a>
     </div>
@@ -61,7 +62,7 @@
               size="icon"
               variant="ghost"
               aria-label="메뉴"
-              style={`--svg: url("${menu}")`}
+              style={`--svgOutline: url("${menu}")`}
             >
               <Menu class="size-6" />
             </Button>
@@ -89,7 +90,7 @@
       :global(Button):hover
         @apply text-(--hero-foreground) bg-(--hero-foreground)/10
       .logo, :global(.menu)
-        @apply before:opacity-100 stroke-0!
+        @apply before:opacity-100
     section
       @apply relative container-x !max-w-full px-4 sm:!px-6 flex justify-between items-start gap-8
       .logo
@@ -97,5 +98,5 @@
       :global(.menu)
         @apply relative before:mask-size-[24px]
       .logo, :global(.menu)
-        @apply before:bg-(--outline-color) before:absolute before:inset-0 before:-z-1 before:opacity-0 before:transition before:[mask-image:var(--svg)] before:mask-center before:mask-no-repeat dark:stroke-background dark:stroke-1
+        @apply before:bg-(--outline-color) before:absolute before:inset-0 before:-z-1 before:opacity-0 before:transition before:mask-(--svg) before:mask-center before:mask-no-repeat dark:mask-(--svgGradeDown) mask-center mask-[size:36px] mask-no-repeat
 </style>
