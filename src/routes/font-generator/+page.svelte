7281a00234c51b30222994df22afe8ceb2bf866f
Minseo Lee <itoupluk427@gmail.com>
2026-02-28 08:30:33 +0900

    Update +page.svelte

--- src/routes/font-generator/+page.svelte
+++ src/routes/font-generator/+page.svelte
@@ -727,18 +727,16 @@
     @apply flex flex-col lg:flex-row-reverse items-start gap-4
 
   .preview-area
-    @apply block flex-1 w-full bg-secondary/50 rounded-lg h-[calc(100vh-3rem)] overflow-hidden sticky top-6
+    @apply block flex-1 w-full min-h-40 bg-secondary/50 rounded-lg h-[calc(100vh-3rem)] overflow-hidden lg:sticky top-6
 
   .canvas-wrapper
     @apply flex items-center justify-center min-w-full min-h-full w-max h-max p-6
-    &.hidden
-      @apply hidden
 
   .preview-canvas
     image-rendering: pixelated
 
   .placeholder
-    @apply absolute inset-0 text-sm text-muted-foreground p-4 flex flex-col items-center justify-center text-center pointer-events-none
+    @apply absolute inset-0 text-sm text-muted-foreground p-6 flex items-center justify-center text-center pointer-events-none
 
   .sidebar
     @apply sticky top-5 flex flex-col w-full lg:w-2xs shrink-0
