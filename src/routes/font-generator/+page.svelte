5b72105d6f6991e779a7ecf7c5fdef0e7d3abeaa
Minseo Lee <itoupluk427@gmail.com>
2026-02-28 07:26:06 +0900

    Update +page.svelte

--- src/routes/font-generator/+page.svelte
+++ src/routes/font-generator/+page.svelte
@@ -684,9 +684,10 @@
     @apply flex flex-col lg:flex-row-reverse items-start gap-4
 
   .preview-area
-    @apply flex flex-1 items-center justify-center w-full bg-secondary/50 rounded-lg h-[calc(100vh-3rem)] p-7 overflow-auto sticky top-6
+    @apply flex flex-1 items-center justify-center w-full bg-secondary/50 rounded-lg h-[calc(100vh-3rem)] overflow-auto sticky top-6
 
   .preview-canvas
+    @apply m-6
     image-rendering: pixelated
     &.hidden
       @apply hidden
