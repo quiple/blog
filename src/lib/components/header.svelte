70b0bfec359c87be31277d14ccaed801864e1407
Minseo Lee <itoupluk427@gmail.com>
2026-02-28 10:34:55 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -69,7 +69,7 @@
           {/snippet}
         </DropdownMenu.Trigger>
         <DropdownMenu.Content align="end">
-          <DropdownMenu.Item>비트맵 폰트 이미지 생성기</DropdownMenu.Item>
+          <DropdownMenu.Item onclick={() => goto('/font-generator')}>비트맵 폰트 이미지 생성기</DropdownMenu.Item>
           <DropdownMenu.Separator />
           <DropdownMenu.Item onclick={toggleMode}>
             {#if mode.current === 'dark'}<Sun /> 라이트 테마
