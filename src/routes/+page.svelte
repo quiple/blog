4cb10f4a6da960713a8ba8ee6b94a8b2218446c0
Minseo Lee <itoupluk427@gmail.com>
2026-03-02 01:38:56 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,6 @@
 </svelte:head>
 
 <div class="max-w-xl 2xl:max-w-2xl mx-auto">
-  <h1 class="text-2xl leading-tight font-bold tracking-tight md:text-3xl lg:text-4xl mb-2 md:mb-4">전체 글</h1>
   <ul class="flex flex-col z-10 relative">
     {#each data.posts as post}
       {@const displayDate =
