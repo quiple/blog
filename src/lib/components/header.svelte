151e20b5d37aefd37a33b78f7ac134e0fbfcb914
Minseo Lee <itoupluk427@gmail.com>
2026-03-01 23:56:31 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -106,7 +106,7 @@
       .logo
         @apply flex items-center gap-1 self-center transition relative before:mask-size-[54px] p-1 -m-1
       :global(.menu)
-        @apply relative before:mask-size-[24px]
+        @apply relative before:mask-size-[24px] print:hidden
       .logo, :global(.menu)
         @apply before:bg-(--outline-color) before:absolute before:inset-0 before:-z-1 before:opacity-0 before:transition before:mask-(--svgOutline) before:mask-center before:mask-no-repeat dark:mask-(--svgGradeDown) mask-center mask-[size:36px] mask-no-repeat
 </style>
