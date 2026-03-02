ddfd50c2b0e2bf56ccdd40f683d9754fa1009a91
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 03:28:48 +0900

    fonts

--- src/routes/api/og/[category]/[slug].png/+server.ts
+++ src/routes/api/og/[category]/[slug].png/+server.ts
@@ -1,5 +1,10 @@
 import {ImageResponse} from '@ethercorps/sveltekit-og'
+import {CustomFont, resolveFonts} from '@ethercorps/sveltekit-og/fonts'
 import {error} from '@sveltejs/kit'
+import {read} from '$app/server'
+import geistBoldFontPath from '$lib/assets/fonts/Geist-Bold.otf?url'
+import plexSansJPBoldFontPath from '$lib/assets/fonts/IBMPlexSansJP-Bold.otf?url'
+import plexSansKRBoldFontPath from '$lib/assets/fonts/IBMPlexSansKR-Bold.otf?url'
 import OgImage from '$lib/components/og/post.svelte'
 import {blogArticles, blogFonts, blogPosts, getAllBlogContentMetadata} from '$lib/content'
 import {processTitle} from '$lib/markdown'
@@ -15,7 +20,21 @@ export const entries: EntryGenerator = () => {
   }))
 }
 
+const geistBold = new CustomFont('My Custom Font', () => read(geistBoldFontPath).arrayBuffer(), {
+  weight: 700,
+})
+
+const plexSansKRBold = new CustomFont('My Custom Font', () => read(plexSansKRBoldFontPath).arrayBuffer(), {
+  weight: 700,
+})
+
+const plexSansJPBold = new CustomFont('My Custom Font', () => read(plexSansJPBoldFontPath).arrayBuffer(), {
+  weight: 700,
+})
+
 export const GET: RequestHandler = async ({params}) => {
+  const resolvedFontOptions = await resolveFonts([geistBold, plexSansKRBold, plexSansJPBold])
+
   const matchPath = `/src/posts/${params.category}/${params.slug}.md`
   const rawContent = blogPosts[matchPath] ?? blogArticles[matchPath] ?? blogFonts[matchPath]
   if (!rawContent) return error(404)
@@ -30,6 +49,7 @@ export const GET: RequestHandler = async ({params}) => {
     {
       width: 1200,
       height: 630,
+      fonts: resolvedFontOptions,
     },
     {title, image, imageForeground},
   )
