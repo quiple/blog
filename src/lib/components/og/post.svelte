4e990321af01f668e00dc517d2f34002fa0833ca
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 01:13:53 +0900

    Update post.svelte

--- src/lib/components/og/post.svelte
+++ src/lib/components/og/post.svelte
@@ -1,13 +1,4 @@
-<svelte:options css="injected" />
-
-<main>
+<main class="flex flex-col justify-center items-center size-full bg-zinc-800">
   <h1 style="color: gray; font-size: 80px; margin: 0;">@ethercorps/sveltekit-og</h1>
   <p style="color: gray; font-size: 36px; margin-top: 20px;">Your Svelte Component Open Graph Image!</p>
 </main>
-
-<style lang="sass">
-  @reference '#app.css'
-
-  main
-    @apply flex flex-col justify-center items-center size-full bg-gray-800
-</style>
