9d3691afa6151ea8b722cde4dabcb7841702cd99
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 03:53:16 +0900

    Update post.svelte

--- src/lib/components/og/post.svelte
+++ src/lib/components/og/post.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import Q from '$lib/components/q.svelte'
 
-  let {title = '', image, imageForeground}: {title?: string; image?: string; imageForeground?: string} = $props()
+  let {
+    title = '',
+    image = '',
+    imageForeground = '',
+  }: {title: string; image?: string; imageForeground?: string} = $props()
 </script>
 
 <main
   class="flex flex-col justify-between items-start bg-white h-full w-full p-12"
   style="color: oklch(0.141 0.005 285.823); font-feature-settings: 'palt' 1, 'ss01' 1, 'ss03' 1"
+  style:background-image={image && `url('${image}')`}
 >
   <Q class="w-20 h-20" />
-  <h1 class="font-bold text-6xl leading-tight -mb-3">{title}</h1>
+  <h1 class="font-bold text-6xl leading-tight -mb-3">{image}</h1>
 </main>
