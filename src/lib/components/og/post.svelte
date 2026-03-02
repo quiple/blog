706012121a4eda3282600e262177be7919bfc367
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 07:06:46 +0900

    Update post.svelte

--- src/lib/components/og/post.svelte
+++ src/lib/components/og/post.svelte
@@ -13,8 +13,8 @@
   let {title, category, image, imageForeground}: Props = $props()
 </script>
 
-<main class="flex flex-col justify-between items-start bg-white h-full w-full p-12 overflow-hidden relative">
+<main class="flex flex-col justify-between items-start bg-white h-full w-full p-12 overflow-hidden relative break-keep">
   <Q class="w-20 h-20" />
-  <img src={`${image}`} />
-  <h1 class="font-bold text-6xl leading-tight -mb-3">{title}</h1>
+  <!-- <img src="/img/font/x12y12pxMaruMinyaHangul.png" alt={title} /> -->
+  <h1 class="font-bold text-6xl leading-tight -mb-3 break-keep">{title}</h1>
 </main>
