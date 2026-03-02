fc0dfcbfb144caca45f675816176a3b098f1061d
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 01:08:00 +0900

    og-image

---
+++ src/routes/img/post-og.png/+server.ts
@@ -0,0 +1,16 @@
+import {ImageResponse} from '@ethercorps/sveltekit-og'
+import type {RequestHandler} from '@sveltejs/kit'
+import SimpleCard from '$lib/components/og/post.svelte'
+
+// This is optional, use it if you want to generate OG image at build time.
+export const prerender = true
+
+export const GET: RequestHandler = async () => {
+  return new ImageResponse(
+    SimpleCard, // ⬅️ Pass the Svelte component here
+    {
+      width: 1200,
+      height: 600,
+    },
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
+import {sveltekitOG} from '@ethercorps/sveltekit-og/plugin'
 import {sveltekit} from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit(), sveltekitOG()],
   assetsInclude: ['**/*.md'],
   resolve: {
     alias: [
