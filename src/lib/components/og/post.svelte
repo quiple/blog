038d5a6dc8cf4f9a25a493aac00bde8b43249a72
Minseo Lee <itoupluk427@gmail.com>
2026-03-03 03:49:30 +0900

    Update post.svelte

--- src/lib/components/og/post.svelte
+++ src/lib/components/og/post.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <main
-  class="flex flex-col justify-center items-center bg-white h-full w-full"
-  style="color: oklch(0.141 0.005 285.823)"
+  class="flex flex-col justify-between items-start bg-white h-full w-full p-12"
+  style="color: oklch(0.141 0.005 285.823); font-feature-settings: 'palt' 1, 'ss01' 1, 'ss03' 1"
 >
-  <Q />
-  <div class="mt-8 text-5xl font-bold text-center max-w-200 leading-tight flex justify-center">{title}</div>
+  <Q class="w-20 h-20" />
+  <h1 class="font-bold text-6xl leading-tight -mb-3">{title}</h1>
 </main>
