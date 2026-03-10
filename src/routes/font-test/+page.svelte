fb7f09de07255f349597f743fde55bb349b9599e
Minseo Lee <itoupluk427@gmail.com>
2026-03-10 21:43:50 +0900

    Delete +page.svelte

--- src/routes/font-test/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div style="font-weight: 300">O아를回췛顰</div>
-<div style="font-weight: 400">O아를回췛顰</div>
-<div style="font-weight: 500">O아를回췛顰</div>
-<div style="font-weight: 600">O아를回췛顰</div>
-<div style="font-weight: 700">O아를回췛顰</div>
-<div style="font-weight: 800">O아를回췛顰</div>
