8d5d71d09935a2e29d53b44303828646edead763
Minseo Lee <itoupluk427@gmail.com>
2026-03-12 19:39:31 +0900

    Update header.ts

--- src/lib/stores/header.ts
+++ src/lib/stores/header.ts
@@ -1,3 +1,7 @@
 import {writable} from 'svelte/store'
 
 export const isHero = writable<boolean | null>(null)
+export const heroColors = writable<{foreground: string | null; outline: string | null}>({
+  foreground: null,
+  outline: null,
+})
