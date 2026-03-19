94c0ac13b44243edcafd3798fb8f325b3d462edf
Minseo Lee <itoupluk427@gmail.com>
2026-03-19 18:37:38 +0900

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -26,5 +26,5 @@ export const load: PageServerLoad = async ({url}) => {
   const start = (currentPage - 1) * PER_PAGE
   const posts = allPosts.slice(start, start + PER_PAGE)
 
-  return {title, description, posts, currentPage, totalPages}
+  return {title, description, posts, currentPage, totalPages, perPage: PER_PAGE}
 }
