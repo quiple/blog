78595da0ba3f70b261ca5f2a42b5ee94796584fb
Minseo Lee <itoupluk427@gmail.com>
2026-04-07 01:51:09 +0900

    Update +page.svelte

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -73,7 +73,7 @@
       bind:value={searchInput}
       oninput={handleSearch}
       onkeydown={handleKeydown}
-      class="z-50 pl-11 h-12 text-lg md:text-xl dark:bg-card"
+      class="z-50 pl-11 h-12 text-lg md:text-xl bg-background dark:bg-card"
       autofocus
     />
   </div>
