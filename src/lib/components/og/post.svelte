384329af699b42ab4c07373d5c8861d502cf2670
Minseo Lee <itoupluk427@gmail.com>
2026-04-07 17:23:47 +0900

    Update post.svelte

--- src/lib/components/og/post.svelte
+++ src/lib/components/og/post.svelte
@@ -31,7 +31,7 @@
   <div class="flex flex-col justify-between items-start h-full w-full p-12">
     <Q class="w-20 h-20" fill={`#${imageForeground}`} />
     <div class="flex flex-col items-start w-full">
-      <span class="px-3 py-0.5 bg-black text-white rounded-full text-2xl">{getCategoryName(category)}</span>
+      <span class="px-3 py-0.5 bg-black text-white rounded-full text-2xl -mb-2.5">{getCategoryName(category)}</span>
       <h1 class="font-bold text-6xl leading-tight -mb-3" style="word-break: keep-all">
         {title}
       </h1>
