1c927e150eb9057392159703287d70765cb3a627
Minseo Lee <itoupluk427@gmail.com>
2026-04-07 12:43:30 +0900

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,14 @@ export type WithoutChild<T> = T extends {child?: any} ? Omit<T, 'child'> : T
 export type WithoutChildren<T> = T extends {children?: any} ? Omit<T, 'children'> : T
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {ref?: U | null}
+
+export function getCategoryName(category: string) {
+  switch (category) {
+    case 'article':
+      return '번역 기사'
+    case 'font':
+      return '폰트'
+    default:
+      return category
+  }
+}
