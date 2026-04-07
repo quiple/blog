821aa4755a9531f46e6c8195d5194b606f95b9a9
Minseo Lee <itoupluk427@gmail.com>
2026-04-07 13:21:58 +0900

    Update +page.svelte

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -155,8 +155,7 @@
       : null}
     use:transition={`post-metadata-${data.slug}`}
   >
-    {getCategoryName(data.category)}&#8194;&bullet;&#8194;
-    {#if data.media}
+    {getCategoryName(data.category)}&#8194;&bullet;&#8194;{#if data.media}
       <a target="_blank" rel="nofollow noreferrer noopener" href={data.source}>
         {data.media}
       </a>&#8194;&bullet;&#8194;
