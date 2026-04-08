cbe1ce988507bfd501e14379f0f7cbf5e0b0a426
Minseo Lee <itoupluk427@gmail.com>
2026-04-08 17:47:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from 'node:path'
 import {sveltekitOG} from '@ethercorps/sveltekit-og/plugin'
 import {sveltekit} from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
