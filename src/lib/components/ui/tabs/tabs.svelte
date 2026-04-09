4ad7ddfcf06cdf568f1750c4c0173bbd2bd6ead6
Minseo Lee <itoupluk427@gmail.com>
2026-04-09 13:46:45 +0900

    shadcn tabs

---
+++ src/lib/components/ui/tabs/tabs.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import {cn} from '$lib/utils.js'
+  import {Tabs as TabsPrimitive} from 'bits-ui'
+
+  let {ref = $bindable(null), value = $bindable(''), class: className, ...restProps}: TabsPrimitive.RootProps = $props()
+</script>
+
+<TabsPrimitive.Root
+  bind:ref
+  bind:value
+  data-slot="tabs"
+  class={cn('gap-2 group/tabs flex data-[orientation=horizontal]:flex-col', className)}
+  {...restProps}
+/>
