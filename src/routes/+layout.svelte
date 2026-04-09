e111c149f3d6134e4e69b596e4e8764dd70b40d0
Minseo Lee <itoupluk427@gmail.com>
2026-04-09 20:55:20 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,6 +53,8 @@
 
   main
     @apply px-4 sm:px-6 min-h-[calc(100vh-var(--header-height)-var(--footer-height))] pb-6 [--header-height:68px] sm:[--header-height:84px] md:-mt-(--header-height) md:pt-6
+    :global(.page-title)
+      @apply mb-4 px-12 sm:mb-6 h-9 leading-9 text-center line-clamp-1 font-semibold text-lg
     :global(section)
       @apply [.hero+&]:pt-[calc(50vh-var(--header-height))] md:[.hero+&]:pt-[calc(50vh-1.5rem)] print:[.hero+&]:pt-[calc(56.25vw-var(--header-height))] print:md:[.hero+&]:pt-[calc(56.25vw-1.5rem)]
       :global(.toc)
