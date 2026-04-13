98a73037f2de5caa5eae17d721fc97e5312b3dbd
Minseo Lee <itoupluk427@gmail.com>
2026-04-14 00:30:07 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -98,8 +98,8 @@
             </div>
           {/if}
           <DropdownMenu.Item onclick={() => goto('/font-generator')}>비트맵 폰트 이미지 생성기</DropdownMenu.Item>
-          <DropdownMenu.Item onclick={() => goto('/message-maker')}>메시지 만들기</DropdownMenu.Item>
           {#if !isProd}
+            <DropdownMenu.Item onclick={() => goto('/message-maker')}>메시지 만들기</DropdownMenu.Item>
             <DropdownMenu.Item onclick={() => goto('/frame-maker')}>프레임 만들기</DropdownMenu.Item>
           {/if}
           <DropdownMenu.Separator />
