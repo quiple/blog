5db4540c5d079cb4231d9424da57ec381721d4b7
Minseo Lee <itoupluk427@gmail.com>
2026-04-14 23:44:54 +0900

    Update +page.svelte

--- src/routes/message-maker/+page.svelte
+++ src/routes/message-maker/+page.svelte
@@ -565,7 +565,7 @@
                 >
                 <Textarea
                   id="msg-input-{i}-{bi}"
-                  class="grow min-h-[38px]"
+                  class="grow min-h-9.5"
                   placeholder={msg.type === 'left' ? '왼쪽 메시지 입력...' : '오른쪽 메시지 입력...'}
                   bind:value={msg.text[bi]}
                   oninput={requestRedraw}
