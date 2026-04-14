ec5acdeed19615ef243cd795b48a557764cfc798
Minseo Lee <itoupluk427@gmail.com>
2026-04-15 00:09:08 +0900

    Update +page.svelte

--- src/routes/message-maker/+page.svelte
+++ src/routes/message-maker/+page.svelte
@@ -43,80 +43,84 @@
   let density: number = $state(2)
   let canvasEl: HTMLCanvasElement | undefined = $state()
   let previewContainer: HTMLDivElement | undefined = $state()
-  let messages: MessageItem[] = $state([
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['안녕하세요, 선생님. 유우카입니다.'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['저 기억하고 계시죠?'],
-    },
-    {
-      type: 'right',
-      text: ['아아. 당연하지.'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['뭐, 그럼 다행이구요.'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: [
-        '선생님의 연락처를 받아두길 잘했네요.',
-        '모모톡으로 연락드린 건 다름이 아니라…….',
-        '지난번 살레 탈환 당시 사용했던 탄환의 경비 처리가 늦어지고 있어서요.',
-      ],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['경비는 언제쯤 청구받을 수 있을까요?'],
-    },
-    {
-      type: 'right',
-      text: ['이쪽에서 처리해야 하는 거였어……?'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: [
-        '물론이죠. 탄환도 공짜는 아니니까요.',
-        '청구서를 작성해서 보내주시면 총학생회에서',
-        '대신 잔금을 치러줄 거예요.',
-      ],
-    },
-    {
-      type: 'right',
-      text: ['청구서는 어떻게 써야 하지…….'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['청구서 양식이라면 밀레니엄 학원에서 쓰는 것이 있어요.', '다음에 샬레를 방문할 때 가져다드릴게요.'],
-    },
-    {
-      type: 'right',
-      text: ['도와줘서 고마워.'],
-    },
-    {
-      type: 'left',
-      name: '유우카',
-      portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
-      text: ['어려운 일도 아닌걸요.', '그럼 좋은 하루 되세요.'],
-    },
-  ])
+  let messages: MessageItem[] = $state(
+    isProd
+      ? []
+      : [
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['안녕하세요, 선생님. 유우카입니다.'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['저 기억하고 계시죠?'],
+          },
+          {
+            type: 'right',
+            text: ['아아. 당연하지.'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['뭐, 그럼 다행이구요.'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: [
+              '선생님의 연락처를 받아두길 잘했네요.',
+              '모모톡으로 연락드린 건 다름이 아니라…….',
+              '지난번 살레 탈환 당시 사용했던 탄환의 경비 처리가 늦어지고 있어서요.',
+            ],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['경비는 언제쯤 청구받을 수 있을까요?'],
+          },
+          {
+            type: 'right',
+            text: ['이쪽에서 처리해야 하는 거였어……?'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: [
+              '물론이죠. 탄환도 공짜는 아니니까요.',
+              '청구서를 작성해서 보내주시면 총학생회에서',
+              '대신 잔금을 치러줄 거예요.',
+            ],
+          },
+          {
+            type: 'right',
+            text: ['청구서는 어떻게 써야 하지…….'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['청구서 양식이라면 밀레니엄 학원에서 쓰는 것이 있어요.', '다음에 샬레를 방문할 때 가져다드릴게요.'],
+          },
+          {
+            type: 'right',
+            text: ['도와줘서 고마워.'],
+          },
+          {
+            type: 'left',
+            name: '유우카',
+            portrait: '/img/blue-archive/Student_Portrait_Yuuka_Collection.png',
+            text: ['어려운 일도 아닌걸요.', '그럼 좋은 하루 되세요.'],
+          },
+        ],
+  )
 
   // 학생 선택 대화 상자
   let showStudentDialog = $state(false)
