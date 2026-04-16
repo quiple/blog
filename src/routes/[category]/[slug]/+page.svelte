9f5108da45c1f15ad749000f89cc04c9bd1aa83c
Minseo Lee <itoupluk427@gmail.com>
2026-04-17 06:57:03 +0900

    Update +page.svelte

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -275,7 +275,7 @@
         .metadata
           @apply text-sm mb-5 inline-block
           a
-            @apply font-normal text-(--hero-foreground)! no-underline hover:underline
+            @apply font-normal text-(--hero-foreground)! no-underline
       &.line h1::before
         @apply content-(--content) absolute -z-1 pr-4 sm:pr-8 md:pr-0
         -webkit-text-stroke: 6px var(--outline-color)
