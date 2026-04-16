b21af5acc67be593cae5de4327ca346b2a44ea3a
Minseo Lee <itoupluk427@gmail.com>
2026-04-17 07:50:57 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -59,12 +59,6 @@
       @apply mb-4 px-12 sm:mb-6 h-9 leading-9 text-center line-clamp-1 font-semibold text-lg
     :global(section)
       @apply [.hero+&]:pt-[calc(50vh-var(--header-height))] md:[.hero+&]:pt-[calc(50vh-1.5rem)] print:[.hero+&]:pt-[calc(56.25vw-var(--header-height))] print:md:[.hero+&]:pt-[calc(56.25vw-1.5rem)]
-      :global(.toc)
-        @apply hidden lg:block noscript:hidden
-        :global(h2)
-          @apply flex items-center text-sm font-semibold text-muted-foreground before:mask-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyLjc1IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXRhYmxlLW9mLWNvbnRlbnRzLWljb24gbHVjaWRlLXRhYmxlLW9mLWNvbnRlbnRzIj48cGF0aCBkPSJNMTYgNUgzIi8+PHBhdGggZD0iTTE2IDEySDMiLz48cGF0aCBkPSJNMTYgMTlIMyIvPjxwYXRoIGQ9Ik0yMSA1aC4wMSIvPjxwYXRoIGQ9Ik0yMSAxMmguMDEiLz48cGF0aCBkPSJNMjEgMTloLjAxIi8+PC9zdmc+')] before:bg-current before:size-3.5 before:block before:mask-contain before:mr-1
-        :global(li)
-          @apply font-medium [&.active]:font-medium! [&.active]:hover:text-current! text-sm! mt-2 text-pretty break-keep dark:[&.active]:text-blue-300 transition
       :global(article)
         @apply z-10 prose-shadcn max-w-xl 2xl:max-w-2xl w-full shrink-0
         :global(.metadata)
