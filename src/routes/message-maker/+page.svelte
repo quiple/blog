e0a43c5ba2971d5b432d1ebb45b9767bcb7de284
Minseo Lee <itoupluk427@gmail.com>
2026-04-28 00:50:18 +0900

    Update +page.svelte

--- src/routes/message-maker/+page.svelte
+++ src/routes/message-maker/+page.svelte
@@ -780,28 +780,28 @@
             </Select.Content>
           </Select.Root>
         </div>
-
-        <div class="grid gap-2">
-          <label class="setting-label" for="setting-lang">
-            {#if lang === 'ja'}言語 (Language)
-            {:else if lang === 'ko'}언어 (Language)
-            {:else}Language
-            {/if}
-          </label>
-          <Select.Root type="single" bind:value={lang}>
-            <Select.Trigger class="w-full" id="setting-lang">
-              {langLabels[lang]}
-            </Select.Trigger>
-            <Select.Content>
-              <Select.Group>
-                <Select.Item value="ko">한국어</Select.Item>
-                <Select.Item value="ja">日本語</Select.Item>
-                <Select.Item value="en">English</Select.Item>
-              </Select.Group>
-            </Select.Content>
-          </Select.Root>
-        </div>
       {/if}
+
+      <div class="grid gap-2">
+        <label class="setting-label" for="setting-lang">
+          {#if lang === 'ja'}言語 (Language)
+          {:else if lang === 'ko'}언어 (Language)
+          {:else}Language
+          {/if}
+        </label>
+        <Select.Root type="single" bind:value={lang}>
+          <Select.Trigger class="w-full" id="setting-lang">
+            {langLabels[lang]}
+          </Select.Trigger>
+          <Select.Content>
+            <Select.Group>
+              <Select.Item value="ko">한국어</Select.Item>
+              <Select.Item value="ja">日本語</Select.Item>
+              <Select.Item value="en">English</Select.Item>
+            </Select.Group>
+          </Select.Content>
+        </Select.Root>
+      </div>
 
       <div class="grid gap-2">
         <label class="setting-label" for="setting-density">PNG 배율</label>
