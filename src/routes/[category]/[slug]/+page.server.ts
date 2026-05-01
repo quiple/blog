ce433c9df7806173cc58a39bc0309061bdd5b12d
Minseo Lee <itoupluk427@gmail.com>
2026-05-02 00:38:41 +0900

    Update +page.server.ts

--- src/routes/[category]/[slug]/+page.server.ts
+++ src/routes/[category]/[slug]/+page.server.ts
@@ -31,7 +31,7 @@ import type {PageServerLoad} from './$types'
 const isProd = import.meta.env.PROD
 const imageSizeMap = imageSizes as Record<string, {width: number; height: number}>
 const widthClassRegex = /(^|\s)w-/
-const IMAGE_WIDTHS = [328, 576, 656, 672, 1152, 1344]
+const IMAGE_WIDTHS = [343, 576, 672, 686, 1152, 1344]
 const DEFAULT_SIZES = '(min-width: 1536px) 672px, (min-width: 1280px) 576px, calc(100vw - 32px)'
 
 function generateSrcSet(src: string, isProd: boolean) {
