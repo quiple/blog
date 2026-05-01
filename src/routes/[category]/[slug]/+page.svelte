489729ba835ee39f00624fc1ac7ff4ef9fdab675
Minseo Lee <itoupluk427@gmail.com>
2026-05-01 22:55:30 +0900

    Update +page.svelte

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -26,7 +26,9 @@
   const isArticle = $derived(data.category === 'article')
   const isFont = $derived(data.category === 'font')
 
-  const image = $derived(data.image ? getImageUrl(data.image, {w: 2560}, isProd) : '')
+  const imageMobile = $derived(data.image ? getImageUrl(data.image, {w: 1280}, isProd) : '')
+  const imageDesktop = $derived(data.image ? getImageUrl(data.image, {w: 2560}, isProd) : '')
+  const image4K = $derived(data.image ? getImageUrl(data.image, {w: 3840}, isProd) : '')
   const thumbnail1x = $derived(data.image ? getImageUrl(data.image, {h: 88}, isProd) : '')
   const thumbnail2x = $derived(data.image ? getImageUrl(data.image, {h: 176}, isProd) : '')
   const thumbnailImage = $derived(
@@ -47,7 +49,7 @@
       '@type': 'Article',
       headline: data.title,
       datePublished: `${publishedDate}+09:00`,
-      image: image && [`${BASE_URL}${image}`],
+      image: imageDesktop && [`${BASE_URL}${imageDesktop}`],
       ...(data.author
         ? {
             author: [
@@ -105,7 +107,7 @@
 
   $effect(() => {
     heroColors.set({
-      foreground: image ? `#${data.imageForeground?.toString() ?? '09090b'}` : null,
+      foreground: imageMobile ? `#${data.imageForeground?.toString() ?? '09090b'}` : null,
       outline: data.outline ? `#${data.outline.toString()}` : null,
     })
     return () =>
@@ -190,10 +192,13 @@
   </div>
 {/snippet}
 
-{#if image}
+{#if imageMobile}
   <div
     class="hero bg"
-    style:background-image={`url('${image}'), ${thumbnailImage}`}
+    style:--image-mobile={`url('${imageMobile}')`}
+    style:--image-desktop={`url('${imageDesktop}')`}
+    style:--image-4k={`url('${image4K}')`}
+    style:--thumbnail-image={thumbnailImage}
     style:background-position={`center calc(${data.imageVerticalAlign ?? 50}% + ${scrollY * 0.5}px)`}
     use:transition={`post-image-${data.slug}`}
   ></div>
@@ -260,6 +265,11 @@
     @apply inset-0 absolute! [print-color-adjust:exact]
     &.bg
       @apply w-[calc(100vw-var(--scrollbar-width))] -z-10 h-[50vh] print:h-[56.25vw] bg-cover inner-b-border print:bg-center!
+      background-image: var(--image-desktop), var(--thumbnail-image)
+      @media (max-width: 1024px)
+        background-image: var(--image-mobile), var(--thumbnail-image)
+      @media (min-width: 2560px)
+        background-image: var(--image-4k), var(--thumbnail-image)
     &.title
       @apply justify-center items-end flex z-10 h-[calc(50vh-var(--header-height))] print:h-[calc(56.25vw-var(--header-height))] w-[calc(36rem+2rem)] sm:w-[calc(36rem+4rem)] max-w-full px-4 sm:px-8 md:px-0 mx-auto md:mx-0 top-(--header-height) md:top-0 md:h-[50vh] print:md:h-[56.25vw] md:w-xl md:2xl:w-2xl md:left-1/2 md:-translate-x-1/2
       & > div
