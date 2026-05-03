34391ce84172e7552902f65a1e219ef615e6e4a0
Minseo Lee <itoupluk427@gmail.com>
2026-05-03 21:38:11 +0900

    Update +page.svelte

--- src/routes/message-maker/+page.svelte
+++ src/routes/message-maker/+page.svelte
@@ -849,7 +849,7 @@
               onclick={() => canvasEl && exportAsPng(canvasEl, density, messages, themeName, lang)}
             >
               <Image />
-              {#if lang === 'ja'}PNGにエクスポート
+              {#if lang === 'ja'}PNGでエクスポート
               {:else if lang === 'ko'}PNG로 내보내기
               {:else}Export as PNG
               {/if}
@@ -874,7 +874,7 @@
               onclick={() => canvasEl && exportAsVectorSvg(messages, themeName, lang)}
             >
               <Spline />
-              {#if lang === 'ja'}SVGにエクスポート
+              {#if lang === 'ja'}SVGでエクスポート
               {:else if lang === 'ko'}SVG로 내보내기
               {:else}Export as SVG
               {/if}
@@ -903,9 +903,9 @@
             {/if}
           </h2>
           <p>
-            {#if lang === 'ja'}PNGで書き出す際の倍率を決定します。
-            {:else if lang === 'ko'}PNG로 내보내기를 할 때의 배율을 결정합니다.
-            {:else}Determines the scale factor when exporting to PNG.
+            {#if lang === 'ja'}「PNGでエクスポート」を行う際の倍率を決定します。
+            {:else if lang === 'ko'}'PNG로 내보내기'를 할 때의 배율을 결정합니다.
+            {:else}Determines the scale factor when using "Export as PNG".
             {/if}
           </p>
           <h2>
@@ -916,19 +916,19 @@
           </h2>
           <ul>
             <li>
-              {#if lang === 'ja'}PNGで書き出す: 画像をPNGファイルとして書き出します。一般的な共有に適しています。
+              {#if lang === 'ja'}PNGでエクスポート: 画像をPNGファイルとして書き出します。一般的な共有に適しています。
               {:else if lang === 'ko'}PNG로 내보내기: 이미지를 PNG 파일로 내보냅니다. 일반적인 공유용으로 적합합니다.
               {:else}Export as PNG: Exports the image as a PNG file. Best for general sharing.
               {/if}
             </li>
             <li>
-              {#if lang === 'ja'}PNGとしてコピー: 画像をPNG形式でクリップボードにコピーします。
+              {#if lang === 'ja'}PNGでコピー: 画像をPNG形式でクリップボードにコピーします。
               {:else if lang === 'ko'}PNG로 복사하기: 이미지를 PNG 파일로 클립보드에 복사합니다.
               {:else}Copy as PNG: Copies the image to the clipboard as a PNG file.
               {/if}
             </li>
             <li>
-              {#if lang === 'ja'}SVGで書き出す: 画像をSVGファイルとして書き出します。印刷用に適しています。
+              {#if lang === 'ja'}SVGでエクスポート: 画像をSVGファイルとして書き出します。印刷用に適しています。
               {:else if lang === 'ko'}SVG로 내보내기: 이미지를 SVG 파일로 내보냅니다. 인쇄용으로 적합합니다.
               {:else}Export as SVG: Exports the image as an SVG file. Best for printing.
               {/if}
