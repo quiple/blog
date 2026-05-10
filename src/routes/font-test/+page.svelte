30cb26703e20d4819d264534696cdfb67e1910d9
Minseo Lee <itoupluk427@gmail.com>
2026-05-10 19:26:11 +0900

    Update +page.svelte

--- src/routes/font-test/+page.svelte
+++ src/routes/font-test/+page.svelte
@@ -5,9 +5,16 @@
 <h2 style="font-weight: 675">한0O아123</h2>
 <h2 style="font-weight: 800">한0O아123</h2>
 
+<h2 class="serif" style="font-weight: 375">한0O아123</h2>
+<h2 class="serif" style="font-weight: 500">한0O아123</h2>
+<h2 class="serif" style="font-weight: 675">한0O아123</h2>
+<h2 class="serif" style="font-weight: 800">한0O아123</h2>
+
 <style lang="sass">
   @reference '#app.css'
 
   h2
     @apply text-9xl
+    .serif
+      @apply font-serif
 </style>
