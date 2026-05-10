28152e187d8fe7ca7e9ec9465383ee1fd45f055e
Minseo Lee <itoupluk427@gmail.com>
2026-05-10 20:09:11 +0900

    Update +page.svelte

--- src/routes/font-test/+page.svelte
+++ src/routes/font-test/+page.svelte
@@ -5,42 +5,42 @@
 
 <h1 class="page-title">폰트 섞어쓰기 테스트</h1>
 
-<h2 class="text-9xl" style="font-weight: 375">한0O아123</h2>
-<h2 class="text-9xl" style="font-weight: 500">한0O아123</h2>
-<h2 class="text-9xl" style="font-weight: 675">한0O아123</h2>
-<h2 class="text-9xl" style="font-weight: 800">한0O아123</h2>
+<h2 class="text-9xl" style="font-weight: 375">한0O아123요.</h2>
+<h2 class="text-9xl" style="font-weight: 500">한0O아123요.</h2>
+<h2 class="text-9xl" style="font-weight: 675">한0O아123요.</h2>
+<h2 class="text-9xl" style="font-weight: 800">한0O아123요.</h2>
 
 <section class="sans">
-  <h2 class="text-sm" style="font-weight: 375">한0O아123</h2>
-  <h2 class="text-base" style="font-weight: 375">한0O아123</h2>
-  <h2 class="text-lg" style="font-weight: 375">한0O아123</h2>
-  <h2 class="text-xl" style="font-weight: 375">한0O아123</h2>
-  <h2 class="text-sm" style="font-weight: 500">한0O아123</h2>
-  <h2 class="text-base" style="font-weight: 500">한0O아123</h2>
-  <h2 class="text-lg" style="font-weight: 500">한0O아123</h2>
-  <h2 class="text-xl" style="font-weight: 500">한0O아123</h2>
-  <h2 class="text-sm" style="font-weight: 675">한0O아123</h2>
-  <h2 class="text-base" style="font-weight: 675">한0O아123</h2>
-  <h2 class="text-lg" style="font-weight: 675">한0O아123</h2>
-  <h2 class="text-xl" style="font-weight: 675">한0O아123</h2>
-  <h2 class="text-sm" style="font-weight: 800">한0O아123</h2>
-  <h2 class="text-base" style="font-weight: 800">한0O아123</h2>
-  <h2 class="text-lg" style="font-weight: 800">한0O아123</h2>
-  <h2 class="text-xl" style="font-weight: 800">한0O아123</h2>
+  <h2 class="text-sm" style="font-weight: 375">한0O아123요.</h2>
+  <h2 class="text-base" style="font-weight: 375">한0O아123요.</h2>
+  <h2 class="text-lg" style="font-weight: 375">한0O아123요.</h2>
+  <h2 class="text-xl" style="font-weight: 375">한0O아123요.</h2>
+  <h2 class="text-sm" style="font-weight: 500">한0O아123요.</h2>
+  <h2 class="text-base" style="font-weight: 500">한0O아123요.</h2>
+  <h2 class="text-lg" style="font-weight: 500">한0O아123요.</h2>
+  <h2 class="text-xl" style="font-weight: 500">한0O아123요.</h2>
+  <h2 class="text-sm" style="font-weight: 675">한0O아123요.</h2>
+  <h2 class="text-base" style="font-weight: 675">한0O아123요.</h2>
+  <h2 class="text-lg" style="font-weight: 675">한0O아123요.</h2>
+  <h2 class="text-xl" style="font-weight: 675">한0O아123요.</h2>
+  <h2 class="text-sm" style="font-weight: 800">한0O아123요.</h2>
+  <h2 class="text-base" style="font-weight: 800">한0O아123요.</h2>
+  <h2 class="text-lg" style="font-weight: 800">한0O아123요.</h2>
+  <h2 class="text-xl" style="font-weight: 800">한0O아123요.</h2>
 </section>
 
-<h2 class="font-serif text-9xl" style="font-weight: 400">한0O아123</h2>
-<h2 class="font-serif text-9xl" style="font-weight: 450">한0O아123</h2>
+<h2 class="font-serif text-9xl" style="font-weight: 400">한0O아123요.</h2>
+<h2 class="font-serif text-9xl" style="font-weight: 450">한0O아123요.</h2>
 
 <section class="serif">
-  <h2 class="font-serif text-sm" style="font-weight: 400">한0O아123</h2>
-  <h2 class="font-serif text-base" style="font-weight: 400">한0O아123</h2>
-  <h2 class="font-serif text-lg" style="font-weight: 400">한0O아123</h2>
-  <h2 class="font-serif text-xl" style="font-weight: 400">한0O아123</h2>
-  <h2 class="font-serif text-sm" style="font-weight: 450">한0O아123</h2>
-  <h2 class="font-serif text-base" style="font-weight: 450">한0O아123</h2>
-  <h2 class="font-serif text-lg" style="font-weight: 450">한0O아123</h2>
-  <h2 class="font-serif text-xl" style="font-weight: 450">한0O아123</h2>
+  <h2 class="font-serif text-sm" style="font-weight: 400">한0O아123요.</h2>
+  <h2 class="font-serif text-base" style="font-weight: 400">한0O아123요.</h2>
+  <h2 class="font-serif text-lg" style="font-weight: 400">한0O아123요.</h2>
+  <h2 class="font-serif text-xl" style="font-weight: 400">한0O아123요.</h2>
+  <h2 class="font-serif text-sm" style="font-weight: 450">한0O아123요.</h2>
+  <h2 class="font-serif text-base" style="font-weight: 450">한0O아123요.</h2>
+  <h2 class="font-serif text-lg" style="font-weight: 450">한0O아123요.</h2>
+  <h2 class="font-serif text-xl" style="font-weight: 450">한0O아123요.</h2>
 </section>
 
 <style lang="sass">
