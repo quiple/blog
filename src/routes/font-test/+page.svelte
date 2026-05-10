0ce834088f07c6980c8ebbf15ec700c0f0dce8bb
Minseo Lee <itoupluk427@gmail.com>
2026-05-10 19:20:30 +0900

    bookk

--- src/routes/font-test/+page.svelte
+++ src/routes/font-test/+page.svelte
@@ -1,2 +1,13 @@
-<h1 style="">한0O아123</h1>
-<h1 style="">한0O아123</h1>
+<h1 class="page-title">폰트 섞어쓰기 테스트</h1>
+
+<h2 style="font-weight: 375">한0O아123</h2>
+<h2 style="font-weight: 500">한0O아123</h2>
+<h2 style="font-weight: 675">한0O아123</h2>
+<h2 style="font-weight: 800">한0O아123</h2>
+
+<style lang="sass">
+  @reference '#app.css'
+
+  h2
+    @apply text-9xl
+</style>
