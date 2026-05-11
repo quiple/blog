f1cc212ff92325337b17c1e177a99d7736cf8fa3
Minseo Lee <itoupluk427@gmail.com>
2026-05-11 14:36:11 +0900

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -63,7 +63,7 @@
   style:--logo-mask-image={logoMaskImage}
 >
   <section>
-    <div class="flex gap-4 items-center">
+    <div class="flex gap-4 items-center w-full">
       <a
         href="/"
         class="logo"
@@ -177,7 +177,7 @@
       .logo
         @apply flex items-center gap-1 self-center transition relative before:mask-size-[54px] p-1 -m-1
       .post-title
-        @apply invisible opacity-0 lg:visible lg:opacity-100 transition font-bold line-clamp-2 w-[calc((100%-36px-1rem-36rem)/2-1.5rem)] 2xl:w-[calc((100%-36px-1rem-42rem)/2-1.5rem)]
+        @apply invisible opacity-0 lg:visible lg:opacity-100 transition font-bold line-clamp-2 w-[calc((100%-36px-1rem-36rem)/2-1.5rem)] 2xl:w-[calc((100%-36px-1rem-42rem)/2-1.5rem)] leading-5 -my-0.5
       :global(.menu)
         @apply relative before:mask-size-[24px] print:hidden size-9
       .logo, :global(.menu)
