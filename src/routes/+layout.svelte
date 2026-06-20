edae79e6f2466e270556116e82282a9acc61b278
Minseo Lee <itoupluk427@gmail.com>
2026-06-21 05:16:51 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import '#app.css'
   import '#fonts.sass'
+  // @ts-ignore
   import '@quiple/blog-fonts'
   import {browser} from '$app/environment'
   import {onNavigate} from '$app/navigation'
