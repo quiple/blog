a4d2ea148a4c9d1489e745665d448c46cb50259b
Minseo Lee <itoupluk427@gmail.com>
2026-07-07 22:39:55 +0900

    Update +page.svelte

--- src/routes/font-generator/+page.svelte
+++ src/routes/font-generator/+page.svelte
@@ -754,7 +754,7 @@
     .hash
       @apply absolute left-3 top-1/2 -translate-y-1/2 text-sm text-muted-foreground z-1 pointer-events-none
     .color-swatch
-      @apply absolute right-1.5 top-1/2 -translate-y-1/2 size-5 rounded inner-border after:rounded
+      @apply absolute right-1.5 top-1/2 -translate-y-1/2 size-5 rounded inner-border
 
   .shadow-grid
     @apply grid grid-cols-3 gap-0 w-fit
