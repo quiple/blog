53a58191ac8330708b0c0a274b28b048e6c7417e
Minseo Lee <itoupluk427@gmail.com>
2026-08-12 22:53:45 +0900

    Update post-list.svelte

--- src/lib/components/post-list.svelte
+++ src/lib/components/post-list.svelte
@@ -128,7 +128,9 @@
             <div class="grow">
               <div class="mb-1 flex items-center gap-1">
                 <div class="-ml-px flex shrink-0 items-center" use:transition={postTransition(post.slug, 'category')}>
-                  <Badge variant="secondary" class="bg-zinc-950/5">{getCategoryName(post.category)}</Badge>
+                  <Badge variant="secondary" class="bg-zinc-950/5 dark:bg-zinc-50/9"
+                    >{getCategoryName(post.category)}</Badge
+                  >
                 </div>
                 <strong class="line-clamp-1 grow" use:transition={postTransition(post.slug, 'title')}
                   >{post.title}</strong
