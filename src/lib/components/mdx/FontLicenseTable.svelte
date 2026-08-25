99037bbb25bf20c1efe4d813b76c1178a7b14d0b
Minseo Lee <itoupluk427@gmail.com>
2026-08-25 21:07:31 +0900

    Update FontLicenseTable.svelte

--- src/lib/components/mdx/FontLicenseTable.svelte
+++ src/lib/components/mdx/FontLicenseTable.svelte
@@ -76,7 +76,7 @@
         <td>
           <strong>
             <span
-              class="inline-block size-4 rounded-full p-0.5 text-white [print-color-adjust:exact] {row.tone ===
+              class="inline-block size-4 rounded-full p-0.5 text-white [print-color-adjust:exact] [&>svg]:size-3 {row.tone ===
               'positive'
                 ? 'bg-green-500'
                 : row.tone === 'caution'
