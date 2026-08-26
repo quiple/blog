bda839492139c9b9066e9718e671a3711756d389
Minseo Lee <itoupluk427@gmail.com>
2026-08-27 03:42:44 +0900

    Update +page.svelte

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -433,7 +433,7 @@
     font-feature-settings: normal;
   }
   .astr-title span {
-    animation: astr-weight-wave 6s ease-in-out calc(var(--astr-letter-index) * -0.35s) infinite both;
+    animation: astr-weight-wave 4s ease-in-out calc(var(--astr-letter-index) * -0.6s) infinite both;
   }
   .hero.title.line h1 {
     @apply relative;
