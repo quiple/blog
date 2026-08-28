4dd99afde3833d15eb57248c452291a30c542d7a
Minseo Lee <itoupluk427@gmail.com>
2026-08-28 16:40:23 +0900

    Update Toc.svelte

--- src/lib/components/mdx/Toc.svelte
+++ src/lib/components/mdx/Toc.svelte
@@ -25,9 +25,9 @@
   }
 
   const VIEWPORT_TOP = 100
-  const RAIL_X = 4
+  const RAIL_X = 2
   const LEVEL_OFFSET = 14
-  const RAIL_INSET = 5
+  const RAIL_INSET = 0
 
   let enabled = $state(false)
   let headings = $state<Heading[]>([])
