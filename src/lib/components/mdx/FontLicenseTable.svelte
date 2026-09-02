f16741523f4a83af5f78560d15f1c10a6aae5f96
Minseo Lee <itoupluk427@gmail.com>
2026-09-02 17:41:31 +0900

    Update FontLicenseTable.svelte

--- src/lib/components/mdx/FontLicenseTable.svelte
+++ src/lib/components/mdx/FontLicenseTable.svelte
@@ -77,7 +77,7 @@
         <td>
           <strong>
             <span
-              class="inline-block size-4 rounded-full p-0.5 align-sub text-white [print-color-adjust:exact] {row.tone ===
+              class="inline-block size-4 rounded-full p-0.5 align-sub text-white [print-color-adjust:exact] [&>svg]:size-3 {row.tone ===
               'positive'
                 ? 'bg-green-500'
                 : row.tone === 'caution'
@@ -85,11 +85,11 @@
                   : 'bg-red-500'}"
             >
               {#if row.tone === 'positive'}
-                <Check class="mt-[0.5px] size-3 stroke-4" aria-hidden="true" />
+                <Check class="mt-[0.5px] stroke-4" aria-hidden="true" />
               {:else if row.tone === 'caution'}
-                <Exclamation class="size-3 stroke-4" aria-hidden="true" />
+                <Exclamation class="stroke-4" aria-hidden="true" />
               {:else}
-                <X class="size-3 stroke-4" aria-hidden="true" />
+                <X class="stroke-4" aria-hidden="true" />
               {/if}
             </span>
             {row.status}
