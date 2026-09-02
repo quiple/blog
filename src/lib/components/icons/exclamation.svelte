68451a56a149b16ed7994b25a7d15ed14e078e5a
Minseo Lee <itoupluk427@gmail.com>
2026-09-02 17:43:59 +0900

    Create exclamation.svelte

---
+++ src/lib/components/icons/exclamation.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type {SVGAttributes} from 'svelte/elements'
+  import {cn} from '$lib/utils'
+
+  let {
+    filled = false,
+    size = 24,
+    class: className,
+    strokeWidth = 2,
+    ...restProps
+  }: SVGAttributes<SVGSVGElement> & {
+    filled?: boolean
+    size?: number | string
+    strokeWidth?: number
+  } = $props()
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill={filled ? 'currentColor' : 'none'}
+  stroke="currentColor"
+  stroke-width={strokeWidth}
+  stroke-linecap="round"
+  stroke-linejoin="miter"
+  class={cn('lucide lucide-chevron-up-icon lucide-chevron-up', className)}
+  {...restProps}
+>
+  <path d="m18 15-6-6-6 6" />
+</svg>
