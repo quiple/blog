14192c99b8a803437bde4bcaeaf04a89eaf47845
Minseo Lee <itoupluk427@gmail.com>
2026-09-13 22:55:46 +0900

    Update LyricPage.svelte

--- src/lib/components/lyrics/LyricPage.svelte
+++ src/lib/components/lyrics/LyricPage.svelte
@@ -91,7 +91,7 @@
 </script>
 
 <div
-  class="mx-auto grid max-w-[1500px] items-start gap-8 pt-8 md:pt-20 lg:grid-cols-[minmax(320px,0.85fr)_minmax(0,1.15fr)] lg:gap-12"
+  class="mx-auto grid max-w-400 items-start gap-8 pt-8 md:pt-20 lg:grid-cols-[minmax(320px,0.85fr)_minmax(0,1.15fr)] lg:gap-12"
 >
   <aside class="min-w-0 lg:sticky lg:top-26">
     {#if source}
